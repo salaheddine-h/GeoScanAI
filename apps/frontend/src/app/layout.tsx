9b84e3f2f68d203f18785b770d9b0de8a6a77e04
Alarick <101067974+salaheddine-h@users.noreply.github.com>
2026-08-22 21:19:43 +0100

    Update layout.tsx

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata, ReactNode } from "next";
+// import type { Metadata, ReactNode } from "next";
+import type { ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
